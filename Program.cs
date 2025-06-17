74b3de6bfd57421b443f0a9b513a80db13e273d4
Onur110504 <onurisikli@gmail.com>
2025-06-17 22:56:05 +0300

    Add files via upload
    Checks age with if-else statement

--- Program.cs
+++ Program.cs
@@ -4,24 +4,16 @@ class Program
 {
     static void Main()
     {
-        int a = 55;
-        int b = 5;
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
 
-        int sum = a + b;
-        int diff = a - b;
-        int product = a * b;
-        double division = (double)a / b;
-
-        bool isEqual = (a == b);
-        bool isGreater = (a > b);
-        bool isLessOrEqual = (a <= b);
-
-        Console.WriteLine("Sum: " + sum);
-        Console.WriteLine("Difference: " + diff);
-        Console.WriteLine("Product: " + product);
-        Console.WriteLine("Division: " + division);
-        Console.WriteLine("Is Equal? " + isEqual);
-        Console.WriteLine("Is Greater? " + isGreater);
-        Console.WriteLine("Is Less or Equal? " + isLessOrEqual);
+        if (age < 18)
+        {
+            Console.WriteLine("You are under age");
+        }
+        else
+        {
+            Console.WriteLine("Enter the site");
+        }
     }
 }
