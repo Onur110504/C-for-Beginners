c4aa7dee11688e82ec1964a96aed56b6b388e851
Onur110504 <onurisikli@gmail.com>
2025-06-17 22:13:24 +0300

    Add files via upload
    Add variables example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Onur";
+        int age = 21;
+        double height = 1.83;
+        bool isStudent = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height + " m");
+        Console.WriteLine("Are you a student? " + isStudent);
+    }
+}
